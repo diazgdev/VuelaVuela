efcb37557f216bf03a6fe7901b295d85b6bca99e
Guillermo Diaz <diazgdev@gmail.com>
2021-11-18 08:33:37 -0600

    Fixed duration type and created example Trip

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_13_210354) do
+ActiveRecord::Schema.define(version: 2021_11_18_141103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "trips", force: :cascade do |t|
+    t.date "date"
+    t.string "destination"
+    t.string "departure"
+    t.string "duration"
+    t.float "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
