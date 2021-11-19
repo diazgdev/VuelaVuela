8a6d76b32d6419716dccc4e584b009a0a1c7f575
Rocio Romero <rocio.aldanaromero@gmail.com>
2021-11-18 22:43:19 -0300

    Create trip

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :trips
+  post "trips", to: "trips#create"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
