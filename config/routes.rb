99742529f1893b87afb0abb170ee09e1596a7cdc
Rocio Romero <rocio.aldanaromero@gmail.com>
2021-11-20 17:15:46 -0300

    Reservation

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :trips
+  resources :trips do
+    resources :reservations, only: %i[new create]
+  end
   post "trips", to: "trips#create"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
